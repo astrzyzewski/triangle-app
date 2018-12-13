67cdf6d44a1d2b08edf21d65a47140709c1d778f
Andrzej Strzyżewski <astrzyzewski5@gmail.com>
2018-12-13 22:37:28 +0100

    Prototype of the React app with an interface mock-up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col, Button, Input } from 'reactstrap';
 
+//TODO: basic form input handling
+//TODO: input data-type validation
+//TODO: input correctness validation
+//TODO: calculate angles
+//TODO: calculate triangle type
+//TODO: output results
+//TODO: draw triangle?
+//TODO: error-handling
+//TODO: unit tests
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Container>
+            <Row>
+                <h3>Triangle App</h3>
+            </Row>
+            <form>
+                <Row>
+                    <Col md="2">Side A: </Col>
+                    <Col md="4"><Input type="text" name="side_a" id="s_a" placeholder="Input length of Side A in cm"/></Col>
+                </Row>
+                <Row>
+                    <Col md="2">Side B: </Col>
+                    <Col md="4"><Input type="text" name="side_b" id="s_b" placeholder="Input length of Side B in cm"/></Col>
+                </Row>
+                <Row>
+                    <Col md="2">Side C: </Col>
+                    <Col md="4"><Input type="text" name="side_c" id="s_c" placeholder="Input length of Side C in cm"/></Col>
+                </Row>
+                <Row>
+                    <Button>Submit</Button>
+                </Row>
+            </form>
+        </Container>
     );
   }
 }
